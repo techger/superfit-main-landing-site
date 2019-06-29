f99cc1272c3821496de5fb0b6f433c6f7438ab17
Leo Kwan <leojwak@gmail.com>
2019-06-29 15:09:46 -0400

    Remove ignored files

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  superfit_build_base_uri: 'https://api.superfitapp.com',
-  superfit_workouts_base_uri: "https://workouts.superfit.dev",
-  base_uri: 'https://superfitapp.com',
-};

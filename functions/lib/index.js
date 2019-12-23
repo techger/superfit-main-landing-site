507a34c1b9f4bd8645849428d1a1151d874c1a54
Leo Kwan <leojwak@gmail.com>
2019-12-23 11:21:14 -0500

    update

--- functions/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-const universal = require(`${process.cwd()}/dist/server`).app;
-exports.ssr = functions.https.onRequest(universal);
-//# sourceMappingURL=index.js.map
